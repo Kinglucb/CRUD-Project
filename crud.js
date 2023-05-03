9cfc7243bd72efc3487ecee43b9ba1b446d08014
Kinglucb <lucas_b_alencar@hotmail.com>
2023-05-02 22:42:01 -0300

    JavaScript Inicial

--- crud.js
+++ crud.js
@@ -6,10 +6,10 @@ function cadastrar() {
     let categoria = document.querySelector("#categoria").value
     
     const tarefa = {
-        titulo: titulo,
-        descricao: descricao,
-        pontos: pontos,
-        categoria: categoria
+        titulo,
+        descricao,
+        pontos,
+        categoria
     }
 
     document.querySelector("#tarefas").innerHTML += gerarCard(tarefa)
@@ -17,7 +17,8 @@ function cadastrar() {
 }
 
 function gerarCard(tarefa){
-    return `<div class="col">
+    return `
+    <div class="col-12 col-md-6 col-lg-3">
     <div class="card">
         <div class="card-header">
         ${tarefa.titulo}
